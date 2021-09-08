5f449d4777f324fd27d195533689672e41a9be39
christina makatura <cnmak@comcast.net>
2021-09-07 21:24:00 -0500

    commit tree d4a7344608d76246a4a5245a78c871b7877667cd

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(element => 
+    element.split(' ').map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(' ')
+  )
+  return tutorials;
 }
+
